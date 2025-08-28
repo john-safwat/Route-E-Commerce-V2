ee6708bc4c206f7b904d7fa043c939b4dbddd2f9
omarosama <11410120211067@stud.cu.edu.eg>
2025-08-28 21:04:17 +0300

    _ add registration Ui _ make data and domain layers

--- lib/core/l10n/translations/app_localizations_ar.dart
+++ lib/core/l10n/translations/app_localizations_ar.dart
@@ -1,6 +1,5 @@
 // ignore: unused_import
 import 'package:intl/intl.dart' as intl;
-
 import 'app_localizations.dart';
 
 // ignore_for_file: type=lint
@@ -86,42 +85,6 @@ class AppLocalizationsAr extends AppLocalizations {
 
   @override
   String get enterYourPassword => 'أدخل كلمة المرور';
-
-  @override
-  String get searchHintText => 'ماذا تبحث عن؟';
-
-  @override
-  String get categories => 'التصنيفات';
-
-  @override
-  String get viewAll => 'عرض الكل';
-
-  @override
-  String get homeAppliance => 'الأجهزة المنزلية';
-
-  @override
-  String get shopNow => 'تسوق الآن';
-
-  @override
-  String get upTo => 'حتى';
-
-  @override
-  String get fistOff => 'خصم 20%';
-
-  @override
-  String get secondOff => 'خصم 25%';
-
-  @override
-  String get thirdOff => 'خصم 30%';
-
-  @override
-  String get advertisement1 => 'على جميع السماعات\nو أجهزة AirPods';
-
-  @override
-  String get advertisement2 => 'على جميع مستحضرات التجميل\nو العناية بالبشرة';
-
-  @override
-  String get advertisement3 => 'على أجهزة اللابتوب\nو الهواتف';
 
   @override
   String get searchHintText => 'ماذا تبحث عنه؟';
